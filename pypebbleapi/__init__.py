fce9cb820bc0b3c99de5eb87e7dcaef1fe582af3
Alessio Bogon <youtux@gmail.com>
2015-08-29 18:01:52 +0200

    Add package info

--- pypebbleapi/__init__.py
+++ pypebbleapi/__init__.py
@@ -1,1 +1,9 @@
+__version__ = '0.1.0'
+
+__author__ = 'Alessio Bogon'
+__email__ = 'youtux@example.com'
+
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2015 Alessio Bogon'
+
 from .timeline import Timeline
